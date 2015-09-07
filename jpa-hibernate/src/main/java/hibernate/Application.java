f5431064d748674d9c3f0754a9577a74dfb47774
ufukomer <ufukomer@hotmail.com>
2015-09-08 01:33:52 +0300

    EntityScan

--- jpa-hibernate/src/main/java/hibernate/Application.java
+++ jpa-hibernate/src/main/java/hibernate/Application.java
@@ -1,12 +1,16 @@
 package hibernate;
 
+import hibernate.models.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.orm.jpa.EntityScan;
 
 /**
  * Created by Ufuk on 08-09-15.
  */
 @SpringBootApplication
+// Not essential, Spring Boot tries to guess the location otherwise
+@EntityScan(basePackageClasses = User.class)
 public class Application {
 
     public static void main(String[] args) {
