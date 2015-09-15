639cc1674e997a94d601312343529108aea327f5
ufukomer <ufukomer@hotmail.com>
2015-09-15 17:40:09 +0300

    Polymer Data Binding

---
+++ polymer-data-binding/src/main/java/polymerbinding/PolymerApplication.java
@@ -0,0 +1,15 @@
+package polymerbinding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Ufuk on 15-09-15.
+ */
+@SpringBootApplication
+public class PolymerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PolymerApplication.class, args);
+    }
+}
---
+++ polymer-data-binding/src/main/java/polymerbinding/PolymerController.java
@@ -0,0 +1,23 @@
+package polymerbinding;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+/**
+ * Created by Ufuk on 15-09-15.
+ */
+@RestController
+public class PolymerController {
+
+    @RequestMapping("/greeting")
+    public Map<String, Object> greeting() {
+        Map<String, Object> model = new HashMap<>();
+        model.put("id", UUID.randomUUID().toString());
+        model.put("content", "Hello");
+        return model;
+    }
+}
