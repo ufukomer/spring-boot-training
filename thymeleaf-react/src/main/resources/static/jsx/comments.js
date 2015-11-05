21deb55c88da599f7341b0eccb1eea160615813f
ufukomer <ufukomer@hotmail.com>
2015-11-05 22:44:07 +0300

    Highcharts with jQuery

--- thymeleaf-react/src/main/resources/static/jsx/comments.js
+++ thymeleaf-react/src/main/resources/static/jsx/comments.js
@@ -40,3 +40,33 @@ ReactDOM.render(
     <CommentTable comments={COMMENTS}/>,
     document.getElementById('content')
 );
+
+// HighChart
+$(function () {
+    $('#chart').highcharts({
+        chart: {
+            type: 'bar'
+        },
+        title: {
+            text: 'Fruit Consumption'
+        },
+        credits: {
+            enabled: false
+        },
+        xAxis: {
+            categories: ['Apples', 'Bananas', 'Oranges']
+        },
+        yAxis: {
+            title: {
+                text: 'Fruit eaten'
+            }
+        },
+        series: [{
+            name: 'Jane',
+            data: [1, 0, 4]
+        }, {
+            name: 'John',
+            data: [5, 7, 3]
+        }]
+    });
+});
